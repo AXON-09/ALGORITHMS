badb775d882f1ef6e7331743053729e7b122bc21
AXON-09 <ashutoshnayak0987@gmail.com>
2026-09-05 00:05:32 +0530

    bianry search

--- SERACHING/Binary_Search(i.e. sorted).py
+++ SERACHING/Binary_Search(i.e. sorted).py
@@ -1,22 +1,24 @@
 def Binary_search(arr, target):
     l = 0
-    r = len(arr)-1
+    r = len(arr) - 1  # Initialize pointers
+
     while l <= r:
-        mid = (l + r) // 2
+        mid = (l + r) // 2  # Find middle index
+
         if arr[mid] == target:
-            return mid
+            return mid  # Target found
+
         if arr[mid] < target:
-            l = mid + 1
-        elif arr[mid] > target:               #else:
-            r = mid - 1
-    return -1
+            l = mid + 1  # Search right half
+
+        elif arr[mid] > target:
+            r = mid - 1  # Search left half
+
+    return "-1"  # Target not found
 
 
-
-
+# Test the function
 arr = [10, 25, 30, 45, 50, 90]
 target = 90
-
 result = Binary_search(arr, target)
-
-print(result)
+print(result)  # Output: 5
